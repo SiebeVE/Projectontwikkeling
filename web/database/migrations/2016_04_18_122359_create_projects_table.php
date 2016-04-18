2434d5b269845f6e545336f86754caa64f04376e
Siebe Vanden Eynden <siebe.vandeneynden@student.kdg.be>
2016-04-18 21:43:29 +0200

    fix gitignore

--- web/database/migrations/2016_04_18_122359_create_projects_table.php
+++ web/database/migrations/2016_04_18_122359_create_projects_table.php
@@ -3,38 +3,35 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProjectsTable extends Migration
+class CreateQuestionsTable extends Migration
 {
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('projects', function (Blueprint $table)
-		{
-			$table->increments('id');
-			$table->integer('user_id')->unsigned();
-			$table->foreign('user_id')
-				->references('id')
-				->on('users')
-				->onDelete('cascade')
-				->onUpdate('cascade');
-			$table->string('name');
-			$table->string('description');
-			$table->softDeletes();
-			$table->timestamps();
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('projects');
-	}
+			$table->integer('phase_id')->unsigned();
+
+			$table->string('sort');
+			$table->string('question');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('questions');
+    }
 }
