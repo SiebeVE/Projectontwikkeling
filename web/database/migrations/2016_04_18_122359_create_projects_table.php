2b322aff4aa444c55be88a1fb81d160db4d3e383
Siebe Vanden Eynden <siebe.vandeneynden@student.kdg.be>
2016-04-18 21:58:13 +0200

    fix project migration - oopsie ... :boom:

--- web/database/migrations/2016_04_18_122359_create_projects_table.php
+++ web/database/migrations/2016_04_18_122359_create_projects_table.php
@@ -5,33 +5,36 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateProjectsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('questions', function (Blueprint $table) {
-            $table->increments('id');
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('projects', function (Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('user_id')->unsigned();
+			$table->foreign('user_id')
+				->references('id')
+				->on('users')
+				->onDelete('cascade')
+				->onUpdate('cascade');
+			$table->string('name');
+			$table->string('description');
+			$table->softDeletes();
+			$table->timestamps();
+		});
+	}
 
-			$table->integer('phase_id')->unsigned();
-
-			$table->string('sort');
-			$table->string('question');
-
-            $table->softDeletes();
-            $table->timestamps();
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::drop('questions');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('projects');
+	}
 }
