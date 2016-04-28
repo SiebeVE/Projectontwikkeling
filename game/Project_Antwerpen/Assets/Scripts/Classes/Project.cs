bbe80511be5bec08e24c336f8185e4fa905637a6
kevin stollaerd <kevin.stollaerd@gmail.com>
2016-04-28 14:43:12 +0200

    Adding Project+Stage Class and LocationService

---
+++ game/Project_Antwerpen/Assets/Scripts/Classes/Project.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Project {
+
+    private string mName, mDescription;
+    private List<Stage> mStages;
+    private Stage mCurrentStage;
+
+    public Project(string name, string description, List<Stage> stages)
+    {
+        mName = name;
+        mDescription = description;
+
+        mStages = stages;
+    }
+
+    #region Properties
+    public string Name
+    {
+        get { return mName; }
+    }
+
+    public string Description
+    {
+        get { return mDescription; }
+    }
+
+    public List<Stage> Stages
+    {
+        get { return mStages; }
+        set { mStages = value; }
+    }
+
+    public Stage CurrentStage
+    {
+        get { return mCurrentStage; }
+        set { mCurrentStage = value; }
+    }
+    #endregion
+
+    /// <summary>
+    /// Determines the current stage of a project.
+    /// </summary>
+    /// <param name="stages">The list of stages of this project.</param>
+    public void DetermineCurrentStage(List<Stage> stages)
+    {
+        for(byte i = 0; i < stages.Count; i++)
+        {
+            if(DateTime.Compare(DateTime.Today, stages[i].BeginDate) > 0 && DateTime.Compare(DateTime.Today, stages[i].EndDate) < 0)
+            {
+                mCurrentStage = stages[i];
+                break;
+            }
+        }
+    }
+}
--- game/Project_Antwerpen/Assets/Scripts/Managers/LocationManager.cs
+++ game/Project_Antwerpen/Assets/Scripts/Managers/LocationManager.cs
@@ -5,6 +5,69 @@ using System.Collections;
 /// Contains logic for determing the device's location.
 /// </summary>
 public class LocationManager {
-    
-    
+
+    // variables for storing latitude and longitude
+    private static float lat, lon;
+
+    #region Properties
+    public static float Latitude
+    {
+        get { return lat; }
+    }
+
+    public static float Longitude
+    {
+        get { return lon; }
+    }
+    #endregion
+
+    #region Methods
+    /// <summary>
+    /// Determine device's current location when the service is enabled.
+    /// </summary>
+    public static void DetermineLocation()
+    {
+        if (NetworkManager.IsConnected)             // there is a connection 
+        {
+            // check if GPS is enabled
+            if (IsLocationServiceEnabled())        // it's enabled
+            {
+                Input.location.Start();
+
+                if(Input.location.status == LocationServiceStatus.Failed)
+                {
+                    // TODO: display warning --> Service access denied
+                    Debug.Log("Service access denied!");
+                }
+                else
+                {
+                    // Pass current location to location variables.
+                    lat = Input.location.lastData.latitude;
+                    lon = Input.location.lastData.longitude;
+                }
+            }
+            else  // Location service is not enabled
+            {
+                // TODO: display warning location service should be enabled
+                Debug.Log("Location service is not enabled.");
+            }
+
+            Input.location.Stop();
+        }
+    }
+
+    /// <summary>
+    /// Check whether Unity's locationservice is enabled.
+    /// </summary>
+    /// <returns>True if the service is enabled. False otherwise.</returns>
+    private static bool IsLocationServiceEnabled()
+    {
+        if (Input.location.isEnabledByUser)
+        {
+            return true;
+        }
+
+        return false;
+    }
+    #endregion
 }
