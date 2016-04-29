8f5a375f6cef48a7509fc733150dd67a1986d1d2
kevin stollaerd <kevin.stollaerd@gmail.com>
2016-04-29 21:07:50 +0200

    Fix responsive map + responsive UI test

--- game/Project_Antwerpen/Assets/Scripts/Managers/MapManager.cs
+++ game/Project_Antwerpen/Assets/Scripts/Managers/MapManager.cs
@@ -1,12 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum MapType
+{
+    roadmap,
+    satellite,
+    terrain,
+    hybrid
+}
+
 public class MapManager {
 
-    private static string mURLaddress = "http://maps.googleapis.com/maps/api/staticmap?center=";
-    private static string API_KEY = "&key=AIzaSyAn26km9c6rfD7sWftyRa29QjwISSsIF9I";
+    private static string mURLaddress = "http://maps.googleapis.com/maps/api/staticmap?center=", API_KEY = "&key=AIzaSyAn26km9c6rfD7sWftyRa29QjwISSsIF9I";
     private static WWW www;
+    private static int zoom, scale;
+    private static MapType maptype;
 
+    #region Properties
     public static string URLaddress
     {
         get { return mURLaddress; }
@@ -19,6 +29,36 @@ public class MapManager {
         set { www = value; }
     }
 
+    /// <summary>
+    /// The zoom factor of the map, how much should the map be zoomed in?
+    /// </summary>
+    /// <remarks>Value range from 1 (world level) to 20 (Buildings level)</remarks>
+    public static int Zoom
+    {
+        get { return zoom; }
+        set { zoom = value; }
+    }
+
+    /// <summary>
+    /// The scale factor of the map, how much detail we want to be showed in the map?
+    /// </summary>
+    /// <remarks>Only value of 1 or 2 is supported in the free version.</remarks>
+    public static int Scale
+    {
+        get { return scale; }
+        set { scale = value; }
+    }
+
+    /// <summary>
+    /// Which type of map do we want to be showed?
+    /// </summary>
+    public static MapType Maptype
+    {
+        get { return maptype; }
+        set { maptype = value; }
+    }
+    #endregion 
+
     public static IEnumerator LoadMap(string URL)
     {
         www = new WWW(URL);
@@ -28,7 +68,6 @@ public class MapManager {
 
     public static void SetAddress()
     {
-        mURLaddress += LocationManager.Latitude + "," + LocationManager.Longitude + "&zoom=13&maptype=satellite&markers=color:red%7Clabel:A%7C" + LocationManager.Latitude + "," + LocationManager.Longitude + "&size=1920x1080" + API_KEY;
-        Debug.Log(mURLaddress);
+        mURLaddress += LocationManager.Latitude + "," + LocationManager.Longitude + "&zoom=13&maptype=roadmap&markers=color:red%7Clabel:A%7C" + LocationManager.Latitude + "," + LocationManager.Longitude + "&size=1920x1080" + API_KEY;
     }
 }
--- game/Project_Antwerpen/Assets/Scripts/Managers/testMap.cs
+++ game/Project_Antwerpen/Assets/Scripts/Managers/testMap.cs
@@ -1,19 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class testMap : MonoBehaviour {
 
-	// Use this for initialization
-	IEnumerator Start () {
-        WWW www = new WWW(MapManager.URLaddress);
-        yield return www;
-
-        Renderer r = GetComponent<Renderer>();
-        r.material.mainTexture = www.texture;
-	}
+    // Use this for initialization
+	void Start () {
+        StartCoroutine(loadImg());
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    IEnumerator loadImg()
+    {
+        WWW www = new WWW(MapManager.URLaddress);
+        yield return www;
+
+        GetComponent<RawImage>().texture = www.texture;
+    }
 }
