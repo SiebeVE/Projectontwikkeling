f3f3768ac11fd7c6fbcd993f238c51ffbe792baf
Siebe Vanden Eynden <siebe.vandeneynden@student.kdg.be>
2016-05-15 22:51:54 +0200

    fix falty migrations again... :boom:

--- web/database/migrations/2016_05_15_171123_add_email_confirmation_fields_to_user_table.php
+++ web/database/migrations/2016_05_15_171123_add_email_confirmation_fields_to_user_table.php
@@ -5,31 +5,27 @@ use Illuminate\Database\Migrations\Migration;
 
 class AddEmailConfirmationFieldsToUserTable extends Migration
 {
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::table('users', function (Blueprint $table)
-		{
-			$table->boolean('verified')->default(false)->after("password");
-			$table->string('token')->nullable()->after("verified");
-			$table->string("tempMail")->nullable()->after("token");
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::table('users', function (Blueprint $table)
-		{
-			$table->dropColumn(["tempEmail", "verified", "token"]);
-		});
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
 }
