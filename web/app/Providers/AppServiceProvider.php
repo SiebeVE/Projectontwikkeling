26997646daa7eaa075bdcaa9bd4914814d657de2
Siebe Vanden Eynden <siebe.vandeneynden@student.kdg.be>
2016-06-01 20:26:54 +0200

    Fixed :boom:

--- web/app/Providers/AppServiceProvider.php
+++ web/app/Providers/AppServiceProvider.php
@@ -7,28 +7,38 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-		view()->composer('*', function ($view) {
-			$view->authenticatedUser = Auth::user();
-			$view->userIsAdmin = Auth::user()->isAdmin();
-			$view->userName = Auth::user()->getName();
+	/**
+	 * Bootstrap any application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		view()->composer('*', function ($view)
+		{
 			$view->userGuest = Auth::guest();
+			if ($view->userGuest)
+			{
+				$view->authenticatedUser = NULL;
+				$view->userIsAdmin = false;
+				$view->userName = NULL;
+			}
+			else
+			{
+				$view->authenticatedUser = Auth::user();
+				$view->userIsAdmin = Auth::user()->isAdmin();
+				$view->userName = Auth::user()->getName();
+			}
 		});
-    }
+	}
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+	/**
+	 * Register any application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
 }
