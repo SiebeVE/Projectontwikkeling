3a7505b0d6d35c07aeae6561fb9839cd68735f70
Denis <denis.inghelbrecht@outlook.be>
2016-06-03 12:59:25 +0200

    merge conflict

--- web/resources/views/projects/overzicht.blade.php
+++ web/resources/views/projects/overzicht.blade.php
@@ -24,6 +24,42 @@
             <h1>Overzicht</h1>
             <h3>Bekijk en beoordeel jouw favoriete projecten</h3>
 
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                    @foreach($projects as $project)
+                        <li data-target="#myCarousel" data-slide-to="{{$project->id}}" ></li>
+                    @endforeach
+                </ol>
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+
+                    <div  class="item active">
+                        <img class="projectImage" src="{{ url('/images/Grote_Markt.jpg') }}" alt="project foto"/>
+                    </div>
+
+                    @foreach($projects as $project)
+                        <div  class="item">
+                            <img class="projectImage" src="{{$project->photo_path}}" alt="project foto"/>
+                        </div>
+                    @endforeach
+                </div>
+
+
+                <!-- Left and right controls -->
+                <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+
                     @foreach($projects as $project)
                         <div class="projectbekijken col-md-12">
                              <div class="col-md-8 projectbekijkeninfo">
