350270a02e244d02a2cf9ce343814ba30e707ce3
Siebe Vanden Eynden <siebe.vandeneynden@student.kdg.be>
2016-06-03 23:00:28 +0200

    wip phases pictures

--- web/public/js/dragLayout.js
+++ web/public/js/dragLayout.js
@@ -1,7 +1,8 @@
 (function ( $ ) {
 	var rangeSlider = document.getElementById('slider');
+	var rangeSliderMedia = document.getElementById('slider-media');
 
-	noUiSlider.create(rangeSlider, {
+	var settingsSlider = {
 		start: [ 1 ],
 		step: 1,
 		connect: 'lower',
@@ -15,7 +16,10 @@
 			density: 25,
 			stepped: true
 		}
-	});
+	};
+
+	noUiSlider.create(rangeSlider, settingsSlider);
+	noUiSlider.create(rangeSliderMedia, settingsSlider);
 
 	var $grid = $('.grid').packery({
 		// options
@@ -36,10 +40,14 @@
 		$("#input-specific").empty();
 	});
 
+	$("#input-specific-media").slideUp(600, function () {
+		$("#input-specific-media").empty();
+	});
+
 	// Wanneer de select van control wordt veranderd
-	$("#sortQuestion").change(function () {
+	$(".control-field").on("change", "#sortQuestion", function () {
 		var $inputPlace = $("#input-specific");
-		var $newElement;
+		console.log("ok");
 		switch ($(this).val()) {
 			case "checkbox":
 				// Toevoegen van eventuele velden
@@ -82,6 +90,46 @@
 		console.log($(this).val());
 	});
 
+	$(".control-field").on("change", "#sortMedia", function () {
+		var $inputPlace = $("#input-specific-media");
+		console.log("ok");
+		switch ($(this).val()) {
+			// <div class="form-group">
+			// 	<label for="question" class="control-label">Vraag</label>
+			// 	<input type="text" id="question" name="question" class="form-control">
+			// 	</div>
+			case "youtube":
+				// Toevoegen van eventuele velden
+				var $div = $("<div>").addClass("form-group");
+				var $label = $("<label>").addClass("control-label").attr("for", "youtube-url").text("Plak hier de Youtube url.");
+				var $input = $("<input>")
+					.attr("type", "text")
+					.addClass("form-control")
+					.attr("name", "youtube-url")
+					.attr("id", "youtube-url")
+					.attr("placeholder", "Youtube url");
+				$div.append($label).append($input);
+				$inputPlace.slideUp(600, function () {
+					$inputPlace.empty().append($div).slideDown();
+				});
+				break;
+			case "picture":
+				break;
+			default:
+				$inputPlace.slideUp(600, function () {
+					$inputPlace.empty();
+				});
+				break;
+		}
+		console.log($(this).val());
+	});
+
+	function emptyForm() {
+		$("#question").val("");
+		$("#sortQuestion").val("choose");
+		$("#sortQuestion").change();
+	}
+
 	$(".control-field").on("click", "#addBlock", function ( e ) {
 		console.log(e);
 		e.preventDefault();
@@ -89,106 +137,150 @@
 
 		// Toevoegen van blok
 
-		// Ophalen blok nummer en wegschrijven
-		var blockNumber = parseInt($("#numberOfFields").val()) + 1;
-		$("#numberOfFields").val(blockNumber);
-
-		var $exampleField = $("#example-field");
-
 		var typeOfField = $("#sortQuestion").val();
-		var widthOfBlock = Math.floor(rangeSlider.noUiSlider.get());
-		//                console.log(rangeSlider.noUiSlider.get());
-		var question = $("#question").val();
-
-		var className = "grid-item--width" + widthOfBlock;
-
-		// Maken van blok
-		var $newBlock = $("<div>").addClass(className)/*.addClass("form-group")*/.addClass("grid-item").data("width", widthOfBlock);
-
-		var inputName = "question-" + blockNumber;
-		var $label;
-		var $input;
-
-		switch (typeOfField) {
-			case "text":
-				console.log("ok-texr");
-				// Toevoegen van inputs
-				$label = $("<label>").addClass("form-label").text(question).attr("for", inputName).data("sort", typeOfField);
-				$input = $("<input>").addClass("form-control").attr("type", "text").attr("id", inputName).attr("name", inputName);
-				console.log($label);
-				console.log($input);
-				break;
-			case "textarea":
-				console.log("ok-area");
-				// Toevoegen van inputs
-				$label = $("<label>").addClass("form-label").text(question).attr("for", inputName).data("sort", typeOfField);
-				$input = $("<textarea>").addClass("form-control").attr("id", inputName).attr("name", inputName);
-				console.log($label);
-				console.log($input);
-				break;
-			case "checkbox":
-				console.log("ok-check");
-				$label = $("<b>").addClass("form-label").text(question).data("sort", typeOfField);
-				$input = $("<div>");
-				// get all posible answers
-				var $inputs = $("#input-specific").find("input");
-				$inputs.each(function () {
-					if ($(this).val() != "") {
-						// Make label and checkbox for each answer
-						var $div = $("<div>").addClass("checkbox");
-						var $checkLabel = $("<label>").text($(this).val());
-						var $checkInput = $("<input>").attr("type", "checkbox").val($(this).val()).attr("name", inputName);
-
-						$checkLabel.prepend($checkInput);
-						$div.append($checkLabel);
-						$input.append($div);
-					}
-				});
-				break;
-			case "radio":
-				console.log("ok-radio");
-				$label = $("<b>").addClass("form-label").text(question).data("sort", typeOfField);
-				$input = $("<div>");
-				// get all posible answers
-				var $inputs = $("#input-specific").find("input");
-				$inputs.each(function () {
-					if ($(this).val() != "") {
-						// Make label and checkbox for each answer
-						var $div = $("<div>").addClass("radio");
-						var $checkLabel = $("<label>").text($(this).val());
-						var $checkInput = $("<input>").attr("type", "radio").val($(this).val()).attr("name", inputName);
-
-						$checkLabel.prepend($checkInput);
-						$div.append($checkLabel);
-						$input.append($div);
-					}
-				});
-				break;
+		console.log("soort: " + typeOfField);
+		if (typeOfField == "" || typeOfField == null) {
+			swal(
+				'Foutje!',
+				'U moet een soort vraag kiezen',
+				'error'
+			)
 		}
+		else {
+			var blockNumber;
+			var $exampleField;
+			var widthOfBlock;
+			var question;
+			var className;
+			var $newBlock;
+			var inputName;
+			var $label;
+			var $input;
+			// Ophalen blok nummer en wegschrijven
+			var isEditing = $(".grid").parent().find(".is-editing").length > 0;
+			if (isEditing) {
+				var $editingBlock = $(".grid").parent().find(".is-editing").first();
+				var $labelEdit = $editingBlock.find(".form-label");
 
-		var $close = $("<div>").addClass("controls");
-		var $edit = $("<i>").addClass("fa").addClass("fa-pencil");
-		var $cross = $("<i>").addClass("cross").addClass("fa").addClass("fa-times");
-		$close.append($edit);
-		$close.append($cross);
+				blockNumber = $labelEdit.data("blocknumber");
 
-		$newBlock.append($close).append($label).append($input);
-		console.log($newBlock);
-		// Append new blok in examples
-		$grid.append($newBlock).packery('addItems', $newBlock);
-		console.log($exampleField);
+				$exampleField = $("#example-field");
 
-		// Make element draggable
-		var draggie = new Draggabilly($newBlock[ 0 ]);
-		// bind drag events to Packery
-		$grid.packery('bindDraggabillyEvents', draggie);
+				widthOfBlock = Math.floor(rangeSlider.noUiSlider.get());
+				console.log(rangeSlider.noUiSlider.get());
+				question = $("#question").val();
 
-		$grid.packery('shiftLayout');
+				$editingBlock.removeClass("grid-item--width" + $editingBlock.data("width"));
 
-		// Empty form
-		$("#question").val("");
-		$("#sortQuestion").val("choose");
-		$("#sortQuestion").change();
+				className = "grid-item--width" + widthOfBlock;
+
+				// Maken van blok
+				$newBlock = $editingBlock.addClass(className)/*.addClass("form-group")*/.addClass("grid-item").data("width", widthOfBlock);
+
+				inputName = "question-" + blockNumber;
+				$newBlock.empty();
+
+				console.log("editingFinish");
+			}
+			else {
+				blockNumber = parseInt($("#numberOfFields").val()) + 1;
+				$("#numberOfFields").val(blockNumber);
+
+				$exampleField = $("#example-field");
+
+				widthOfBlock = Math.floor(rangeSlider.noUiSlider.get());
+				console.log(rangeSlider.noUiSlider.get());
+				question = $("#question").val();
+
+				className = "grid-item--width" + widthOfBlock;
+
+				// Maken van blok
+				$newBlock = $("<div>").addClass(className)/*.addClass("form-group")*/.addClass("grid-item").data("width", widthOfBlock);
+
+				inputName = "question-" + blockNumber;
+			}
+			switch (typeOfField) {
+				case "text":
+					console.log("ok-texr");
+					// Toevoegen van inputs
+					$label = $("<label>").addClass("form-label").text(question).attr("for", inputName).data("sort", typeOfField).data("blocknumber", blockNumber);
+					$input = $("<input>").addClass("form-control").attr("type", "text").attr("id", inputName).attr("name", inputName);
+					console.log($label);
+					console.log($input);
+					break;
+				case "textarea":
+					console.log("ok-area");
+					// Toevoegen van inputs
+					$label = $("<label>").addClass("form-label").text(question).attr("for", inputName).data("sort", typeOfField).data("blocknumber", blockNumber);
+					$input = $("<textarea>").addClass("form-control").attr("id", inputName).attr("name", inputName);
+					console.log($label);
+					console.log($input);
+					break;
+				case "checkbox":
+					console.log("ok-check");
+					$label = $("<b>").addClass("form-label").text(question).data("sort", typeOfField).data("blocknumber", blockNumber);
+					$input = $("<div>");
+					// get all posible answers
+					var $inputs = $("#input-specific").find("input");
+					$inputs.each(function () {
+						if ($(this).val() != "") {
+							// Make label and checkbox for each answer
+							var $div = $("<div>").addClass("checkbox");
+							var $checkLabel = $("<label>").text($(this).val());
+							var $checkInput = $("<input>").attr("type", "checkbox").val($(this).val()).attr("name", inputName);
+
+							$checkLabel.prepend($checkInput);
+							$div.append($checkLabel);
+							$input.append($div);
+						}
+					});
+					break;
+				case "radio":
+					console.log("ok-radio");
+					$label = $("<b>").addClass("form-label").text(question).data("sort", typeOfField).data("blocknumber", blockNumber);
+					$input = $("<div>");
+					// get all posible answers
+					var $inputs = $("#input-specific").find("input");
+					$inputs.each(function () {
+						if ($(this).val() != "") {
+							// Make label and checkbox for each answer
+							var $div = $("<div>").addClass("radio");
+							var $checkLabel = $("<label>").text($(this).val());
+							var $checkInput = $("<input>").attr("type", "radio").val($(this).val()).attr("name", inputName);
+
+							$checkLabel.prepend($checkInput);
+							$div.append($checkLabel);
+							$input.append($div);
+						}
+					});
+					break;
+			}
+
+			var $close = $("<div>").addClass("controls");
+			var $edit = $("<i>").addClass("fa").addClass("fa-pencil");
+			var $cross = $("<i>").addClass("cross").addClass("fa").addClass("fa-times");
+			$close.append($edit);
+			$close.append($cross);
+
+			$newBlock.append($close).append($label).append($input);
+			console.log($newBlock);
+
+			if (!isEditing) {
+				// Append new blok in examples
+				$grid.append($newBlock).packery('addItems', $newBlock);
+				console.log($exampleField);
+
+				// Make element draggable
+				var draggie = new Draggabilly($newBlock[ 0 ]);
+				// bind drag events to Packery
+				$grid.packery('bindDraggabillyEvents', draggie);
+			}
+
+			$grid.packery('shiftLayout');
+
+			// Empty form
+			$("#cancel-block").trigger("click");
+		}
 	});
 
 	// show item order after layout
@@ -247,90 +339,109 @@
 			rangeSlider.noUiSlider.set(1);
 		}
 	});
+	rangeSliderMedia.noUiSlider.on('slide', function () {
+		if (Math.floor(rangeSlider.noUiSlider.get()) == 0) {
+			rangeSlider.noUiSlider.set(1);
+		}
+	});
 
 	$("form[name=createPhase]").submit(function ( e ) {
 		e.preventDefault();
 		e.stopPropagation();
-		console.log("Form submited");
-		var itemElems = $grid.packery('getItemElements');
+		swal({
+			title: 'Fase toevoegen',
+			text: "Ben je zeker dat je de fase wil toevoegen?",
+			type: 'warning',
+			showCancelButton: true,
+			confirmButtonColor: '#3085d6',
+			cancelButtonColor: '#d33',
+			confirmButtonText: 'Ja, toevoegen die handel!',
+			cancelButtonText: 'Nee, ik ben nog iets vergeten!',
+			confirmButtonClass: 'btn btn-success',
+			cancelButtonClass: 'btn btn-danger',
+			buttonsStyling: false
+		}).then(function ( isConfirm ) {
+			if (isConfirm === true) {
+				console.log("Form submited");
+				var itemElems = $grid.packery('getItemElements');
 
-		// Get height of parent
-		var parentHeight = $("#example-field").css("height");
-		var formData = {
-			parentHeight: parentHeight,
-			elements: {}
-		};
-		console.log(formData);
-		// Make json of the elements
-		$(itemElems).each(function ( i, itemElem ) {
-			var $question = $(itemElem).find(".form-label").first();
-			var $answers = $question.next();
-			console.log($answers);
-			var questionText = $question.text();
-			var sortQuestion = $question.data("sort");
+				// Get height of parent
+				var parentHeight = $("#example-field").css("height");
+				var formData = {
+					parentHeight: parentHeight,
+					elements: {}
+				};
+				console.log(formData);
+				// Make json of the elements
+				$(itemElems).each(function ( i, itemElem ) {
+					var $question = $(itemElem).find(".form-label").first();
+					var $answers = $question.next();
+					console.log($answers);
+					var questionText = $question.text();
+					var sortQuestion = $question.data("sort");
 
-			var leftOffset = ((parseInt($(itemElem).css("left")) / $("#example-field").width() * 100).toFixed(4)) + "%";
-			var topOffset = $(itemElem).css("top");
+					var leftOffset = ((parseInt($(itemElem).css("left")) / $("#example-field").width() * 100).toFixed(4)) + "%";
+					var topOffset = $(itemElem).css("top");
 
-			var width = $(itemElem).data("width");
+					var width = $(itemElem).data("width");
 
 
-			var newElement = {
-				sort: sortQuestion,
-				question: questionText,
-				options: {
-					left: leftOffset,
-					top: topOffset,
-					width: width
-				}
-			};
+					var newElement = {
+						sort: sortQuestion,
+						question: questionText,
+						options: {
+							left: leftOffset,
+							top: topOffset,
+							width: width
+						}
+					};
 
-			console.log(newElement);
+					console.log(newElement);
 
-			switch (sortQuestion) {
-				case "text":
-					break;
-				case "textarea":
-					break;
-				case "checkbox":
-					newElement.answers = {};
-					// Loop through divs with checkbox
-					$answers.children().each(function ( i ) {
-						console.log("antwoord: " + i);
-						var answer = $(this).find("label").first().text();
-						console.log(answer);
-						newElement.answers[ i ] = answer;
-					});
-					break;
-				case "radio":
-					newElement.answers = {};
-					// Loop through divs with checkbox
-					$answers.children().each(function ( i ) {
-						console.log("antwoord: " + i);
-						var answer = $(this).find("label").first().text();
-						console.log(answer);
-						newElement.answers[ i ] = answer;
-					});
-					break;
+					switch (sortQuestion) {
+						case "text":
+							break;
+						case "textarea":
+							break;
+						case "checkbox":
+							newElement.answers = {};
+							// Loop through divs with checkbox
+							$answers.children().each(function ( i ) {
+								console.log("antwoord: " + i);
+								var answer = $(this).find("label").first().text();
+								console.log(answer);
+								newElement.answers[ i ] = answer;
+							});
+							break;
+						case "radio":
+							newElement.answers = {};
+							// Loop through divs with checkbox
+							$answers.children().each(function ( i ) {
+								console.log("antwoord: " + i);
+								var answer = $(this).find("label").first().text();
+								console.log(answer);
+								newElement.answers[ i ] = answer;
+							});
+							break;
+					}
+
+					console.log("*******************");
+					console.log(newElement);
+
+					formData.elements[ i ] = newElement;
+				});
+				console.log(formData);
+
+				// Make dummy form for sending builded json
+				var $dummyForm = $("<form>").attr("method", "POST");
+				var $dummyInput = $("<input>").attr("name", "data").val(JSON.stringify(formData));
+				// Fetch the csrf token
+				var $token = $("input[name=_token]").clone();
+				$dummyForm.append($dummyInput).append($token);
+				$("body").append($dummyForm);
+				$dummyForm.submit();
 			}
-
-			console.log("*******************");
-			console.log(newElement);
-
-			formData.elements[ i ] = newElement;
 		});
-		console.log(formData);
-
-		// Make dummy form for sending builded json
-		var $dummyForm = $("<form>").attr("method", "POST");
-		var $dummyInput = $("<input>").attr("name", "data").val(JSON.stringify(formData));
-		// Fetch the csrf token
-		var $token = $("input[name=_token]").clone();
-		$dummyForm.append($dummyInput).append($token);
-		$("body").append($dummyForm);
-		$dummyForm.submit();
-
-
 	});
 
 	/*******************************CLICK EVENT********************************/
@@ -339,10 +450,71 @@
 		.on("click", ".controls i.fa.fa-times", function () {
 			$(this).parent().parent().fadeOut(500, function () {
 				$(this).remove();
+				$grid.packery('shiftLayout');
 			});
 		})
-		.on("click", ".controls i.fa.fa-pencil", function(){
-			console.log("edit");
+		.on("click", ".controls i.fa.fa-pencil", function () {
+			$("#addBlock").text("Blok aanpassen");
+			$("#cancel-block").show();
+			var $label = $(this).parent().next();
+			var $gridItem = $label.parent();
+			$gridItem.parent().find(".is-editing").removeClass("is-editing");
+			$gridItem.addClass("is-editing");
+			var sortQuestion = $label.data("sort");
+
+			$("#question").val($label.text());
+			$("#sortQuestion").val(sortQuestion);
+			$("#sortQuestion").change();
+
+			var counter = 1;
+			if (sortQuestion == "radio" || sortQuestion == "checkbox") {
+				var $options = $label.next("div").find("div");
+
+				console.log("options");
+				console.log($options);
+
+				$options.each(function () {
+					counter++;
+					var choice = $(this).find("input").val();
+					var $lastChoice = $(".choice").last();
+					$lastChoice.find("input").val(choice);
+					var $choice = $(".choice").first().clone();
+					var $clonedInput = $choice.find("input");
+					$clonedInput.attr("name", "choice-" + counter).attr("id", "choice-" + counter).val("");
+
+					$choice.hide();
+					$(".choices").append($choice).data("numberOfChoices", counter);
+					$choice.slideDown();
+				});
+			}
+
+			console.log(sortQuestion);
 		});
+
+	$(".control-field").on("click", "#cancel-block", function () {
+		emptyForm();
+		$("#addBlock").text("Blok toevoegen");
+		$("#cancel-block").hide();
+		$(".grid").parent().find(".is-editing").removeClass("is-editing");
+	});
+
+	$(".control-field").on("click", ".form-group div", function () {
+		if ($(".grid").parent().find(".is-editing").length > 0) {
+			swal(
+				'Kan niet wijzigen!',
+				'U kan niet van type wijzigen, verwijder dan deze blok en voeg er 1 toe van het juiste type.',
+				'info'
+			)
+		}
+		else if (!$(this).hasClass("active")) {
+			var $triggerdDiv = $(this);
+			var prev = $triggerdDiv.data("sort") == "question" ? "media" : "question";
+			$("#" + prev + "-control").slideUp(400, function () {
+				$("#" + $triggerdDiv.data("sort") + "-control").slideDown();
+			});
+			$(this).parent().find(".active").removeClass("active");
+			$(this).addClass("active");
+		}
+	});
 
 })(jQuery);
--- web/resources/views/projects/phase/add.blade.php
+++ web/resources/views/projects/phase/add.blade.php
@@ -1,81 +1,127 @@
 @extends('layouts.app')
 
 @section('pageCss')
-    <link rel="stylesheet" href="//cdn.jsdelivr.net/bootstrap.tagsinput/0.4.2/bootstrap-tagsinput.css" />
-    <link href="{{ url('/') }}/css/main.css" rel="stylesheet">
+	<link rel="stylesheet" href="//cdn.jsdelivr.net/bootstrap.tagsinput/0.4.2/bootstrap-tagsinput.css"/>
+	<link href="{{ url('/') }}/css/main.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/sweetalert2/3.2.3/sweetalert2.min.css">
 @endsection
 
 @section('content')
 
-    <div id="banner" >
-        <div id="banner-wrapper" class="clearfix">
-            <div class="banner-slogan">
-                <p>Atypisch Antwerpen</p>
+	<div id="banner">
+		<div id="banner-wrapper" class="clearfix">
+			<div class="banner-slogan">
+				<p>Atypisch Antwerpen</p>
 
-            </div>
-            <div class="banner-text">
-                <p>Projecten</p>
-                <p>in jouw</p>
-                <p>buurt</p>
-            </div>
-        </div>
-    </div>
+			</div>
+			<div class="banner-text">
+				<p>Projecten</p>
+				<p>in jouw</p>
+				<p>buurt</p>
+			</div>
+		</div>
+	</div>
 
-    <div class="container containerPhase">
-        <div class="col-md-12">
-            <h1>{{ $phase->name }} maken</h1>
-            <p class="text-muted">Van {{ date("d/m/Y", strtotime($phase->start)) }}
-                tot {{ date("d/m/Y", strtotime($phase->end)) }}</p>
-            @if(count($errors) > 0)
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-            <form name="createPhase" method="POST">
-                {!! csrf_field() !!}
-                <input type="hidden" name="numberOfFields" id="numberOfFields" value="0">
-                <div id="example-field" class="grid">
-                    <div class="gutter-sizer"></div>
-                    <div class="grid-size"></div>
-                </div>
-                <div class="col-md-12 control-field">
-                    <div class="form-group">
-                        <label for="sortQuestion">Kies je soort vraag</label>
-                        <select id="sortQuestion" name="sortQuestion">
-                            <option value="choose" selected disabled>Maak een keuze</option>
-                            <option value="text">Open vraag 1 regel</option>
-                            <option value="textarea">Open vraag meerdere regels</option>
-                            <option value="checkbox">Meerkeuze vraag</option>
-                            <option value="radio">Enkele keuze</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="question" class="control-label">Vraag</label>
-                        <input type="text" id="question" name="question" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="blockWidth">Breedte van blok</label>
-                        <div id="slider"></div>
-                    </div>
-                    <div id="input-specific"></div>
-                    <a id="addBlock" class="btn btn-success">Blok toevoegen</a>
-                </div>
-                <button type="submit" class="btn btn-succes createButton pull-right">Fase toevoegen</button>
-            </form>
-        </div>
-    </div>
+	<div class="container containerPhase">
+		<div class="col-md-12">
+			<h1>{{ $phase->name }} maken</h1>
+			<p class="text-muted">Van {{ date("d/m/Y", strtotime($phase->start)) }}
+				tot {{ date("d/m/Y", strtotime($phase->end)) }}</p>
+			@if(count($errors) > 0)
+				<div class="alert alert-danger">
+					<ul>
+						@foreach($errors->all() as $error)
+							<li>{{ $error }}</li>
+						@endforeach
+					</ul>
+				</div>
+			@endif
+			<form name="createPhase" method="POST">
+				{!! csrf_field() !!}
+				<input type="hidden" name="numberOfFields" id="numberOfFields" value="0">
+				<div id="example-field" class="grid">
+					<div class="gutter-sizer"></div>
+					<div class="grid-size"></div>
+				</div>
+				<div class="col-md-12 control-field">
+					<div class="sort-control">
+						<div class="form-group">
+							<b class="form-label">Wat wil je toevoegen?</b>
+							<div class="active" data-sort="question"><span><i class="fa fa-comment-o"
+																			  aria-hidden="true"></i> Vragen</span>
+							</div>
+							<div data-sort="media"><span><i class="fa fa-picture-o" aria-hidden="true"></i> Media</span>
+							</div>
+						</div>
+					</div>
+					<div id="question-control">
+						<div class="form-group">
+							<label for="sortQuestion">Kies je soort vraag</label>
+							<select id="sortQuestion" name="sortQuestion">
+								<option value="choose" selected disabled>Maak een keuze</option>
+								<option value="text">Open vraag 1 regel</option>
+								<option value="textarea">Open vraag meerdere regels</option>
+								<option value="checkbox">Meerkeuze vraag</option>
+								<option value="radio">Enkele keuze</option>
+							</select>
+						</div>
+						<div class="form-group">
+							<label for="question" class="control-label">Vraag</label>
+							<input type="text" id="question" name="question" class="form-control">
+						</div>
+						<div class="form-group">
+							<label for="blockWidth">Breedte van blok</label>
+							<div id="slider"></div>
+						</div>
+						<div id="input-specific"></div>
+						<a id="addBlock" class="btn btn-success">Blok toevoegen</a>
+						<a id="cancel-block" class="text-danger">Annuleer blok aanpassen</a>
+					</div>
+					<div id="media-control">
+						<div class="form-group">
+							<label for="sortMedia">Kies je soort media</label>
+							<select id="sortMedia" name="sortMedia">
+								<option value="choose" selected disabled>Maak een keuze</option>
+								<option value="picture">Foto</option>
+								<option value="youtube">Video - Youtube</option>
+							</select>
+						</div>
+						<div class="form-group">
+						</div>
+						<div class="form-group">
+							<label for="blockWidthMedia">Breedte van blok</label>
+							<div id="slider-media"></div>
+						</div>
+						<div id="input-specific-media"></div>
+						<a id="addBlockMedia" class="btn btn-success">Blok toevoegen</a>
+						<a id="cancel-block-media" class="text-danger">Annuleer blok aanpassen</a>
+					</div>
+				</div>
+				<button type="submit" class="btn btn-succes createButton pull-right">Fase toevoegen</button>
+			</form>
+		</div>
+	</div>
 @endsection
 
 @section('pageJs')
-    {{--<script src="https://npmcdn.com/draggabilly@2.1/dist/draggabilly.pkgd.min.js"></script>--}}
-    <script src="{{ url('/') }}/js/draggabilly.pkgd.js"></script>
-    <script src="{{ url('/') }}/js/packery.pkgd.min.js"></script>
-    {{--<script src="{{ url('/') }}/js/packery.pkgd.js"></script>--}}
-    <script src="{{ url('/') }}/js/nouislider.min.js"></script>
-    <script src="{{ url('/js/dragLayout.js') }}"></script>
+	<script src="https://cdn.jsdelivr.net/sweetalert2/3.2.3/sweetalert2.min.js"></script>
+	<script src="{{ url('/js/draggabilly.pkgd.js') }}"></script>
+	<script src="{{ url('/js/packery.pkgd.min.js') }}"></script>
+	<script src="{{ url('/js/nouislider.min.js') }}"></script>
+	<script src="{{ url('/js/dragLayout.js') }}"></script>
+	<script>
+		(function ( $ ) {
+			function checkYoutubeUrl( url ) {
+				var regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+				var match = url.match(regExp);
+				if (match && match[ 2 ].length == 11) {
+					return match[ 2 ];
+				}
+				else {
+					return null
+				}
+			}
+		})(jQuery);
+	</script>
 
 @endsection
